144ec075a368daf9f1dbae75a4b857cfd77c54f0
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2021-10-19 14:55:40 -0300

    Socket.io added and new route created
    The new route returns the last three messages sent

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import cors from "cors";
 
+import { Server, Socket } from "socket.io";
 import { router } from "./routes"
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket=>{
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
 app.use(router)
 
 app.get("/github", (request, response)=>{
@@ -18,5 +33,4 @@ app.get("/signin/callback", (request, response)=>{
   return response.json(code);
 });
 
-app.listen(4000, ()=> console.log("Server is running on PORT 4000"));
-
+export { serverHttp, io };
